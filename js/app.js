7f8fe9009fa73f943ab62f1f223189fcabae53fa
Rhiannon Mortensen <mortensenrhi@gmail.com>
2018-03-06 16:22:13 -0800

    js edited at bottom with a new statement. index yet to be updated with new question. css got new purple.

--- js/app.js
+++ js/app.js
@@ -14,13 +14,19 @@ var userHello = alert('Why hello there ' + userName +'. Hope you are ready for s
 
 var petQuestion = parseInt(prompt('How many pets do you think I have? Please enter a number.'));
 
+console.log(userName + ' answered ' + petQuestion + ' to my pet amount.')
+
+
 if (petQuestion === 2) {
   alert('correct!');
 } else {
   alert('nope, sorry. I have 2 pets');
 }
-console.log(userName + ' answered ' + petQuestion + ' to my pets amoount.')
+
 var myAge = prompt('Do you think I am older than 25? Please enter "yes" or "no".').toLocaleLowerCase();
+
+console.log(myAge + ' was guessed by '+ userName + ' for my age.');
+
 // all the variations of yes/no
 
 if (myAge === 'yes') {
@@ -28,15 +34,15 @@ if (myAge === 'yes') {
 } else {
   alert('Alright! Good job guessing!');
 }
-console.log(myAge + ' was guessed by '+ userName + ' for my age.');
-
 var realName = confirm('Rhi. Is this a nickname? \'Okay\' = \'yes\', \'cancel\' = \'no\'.');
-console.log(realName + ' was guessed by ' + userName + ' for my nickname.')
+console.log(realName + ' was guessed by ' + userName + ' for my nickname.');
 
 if (realName === true) {
   alert('You are definitely right! My full name is Rhiannon.')
 } else {
   alert('WaH wWwAhHhHh! Nope, it is a nickname for Rhiannon.')
 }
-console.log(userName + ' answered for the nickname question.');
 
+var homeStatus = prompt('who do you think I live with? 1) mom and dad, 2) mom, dad and grandpa, 3) alone? Answer\' 1\', \'2\', or \'3\'');
+
+console.log(userName + ' answered '+ homeStatus + ' for homeStatus')
